5030808299d121529685bede09e46756e410da67
John Tucker <john@larkintuckerllc.com>
2020-09-24 08:03:58 -0400

    knex

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const { Client } = require('pg');
+const knex = require('knex');
+const pg = require('pg');
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 3000;
+
+pg.defaults.ssl = true;
 
 if (DATABASE_URL === undefined) {
   console.log('missing environment variables');
   process.exit(1);
 }
 
-const client = new Client({
-  connectionString: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+const client = knex({
+  client: 'pg',
+  connection: DATABASE_URL,
 });
 
 (async () => {
   try {
-    await client.connect();
     const app = express();
 
     app.get('/', (req, res) => res.send('Hello World!'));
 
     app.get('/db', async (req, res) => {
       try {
-        const result = await client.query('SELECT $1::text as message', ['Hello db!']);
-        const message = result.rows[0].message;
-        res.send(message);
-      } catch (queryErr) {
-        console.log(queryErr);
+        const rows = await client.select('lastname')
+          .from('persons')
+        console.log(rows);
+        res.send('success');
+      } catch (err) {
+        console.log(err);
         res.status(500).send('query failed');
       }
     });
